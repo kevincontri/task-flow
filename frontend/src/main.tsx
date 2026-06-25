6bc9aa3a00d3821c3c72051f8830fd1b35e41fe4
kevincontri <kevin.lucas.contri@gmail.com>
2026-06-25 12:13:06 -0300

    debugging

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 async function enableMocking() {
-  return; // Disable MSW for now.
+  //return; // Disable MSW for now.
   // @ts-ignore
   if (import.meta.env.MODE !== "development") return;
   const { worker } = await import("./mocks/browser.js");
